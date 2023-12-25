5ad1cd7e09191054b7d9419425e23d0055e03127
Morar Andrei - Alexandru <48377708+bruhitsmeray@users.noreply.github.com>
2023-12-25 03:27:20 +0200

    Add ImGui plugin

--- Source/Aleph/Aleph.Build.cs
+++ Source/Aleph/Aleph.Build.cs
@@ -8,6 +8,6 @@ public class Aleph : ModuleRules
 	{
 		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
+		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput", "ImGui" });
 	}
 }
