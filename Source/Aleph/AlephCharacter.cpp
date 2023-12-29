19ae4d548b45aaa7fd289b92ca281e7c6a1fa68e
Morar Andrei - Alexandru <48377708+bruhitsmeray@users.noreply.github.com>
2023-12-29 03:56:42 +0200

    Rewrite the Character code

---
+++ Source/Aleph/AlephCharacter.cpp
@@ -0,0 +1,116 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "AlephCharacter.h"
+#include "imgui.h"
+
+#include "Kismet/KismetMathLibrary.h"
+#include "Kismet/KismetMaterialLibrary.h"
+
+#include "Camera/CameraComponent.h"
+#include "Components/CapsuleComponent.h"
+#include "Character/PBPlayerMovement.h"
+
+#include "HealthComponent.h"
+#include "StaminaComponent.h"
+
+AAlephCharacter::AAlephCharacter(const FObjectInitializer& ObjectInitializer) : Super(ObjectInitializer.SetDefaultSubobjectClass<UPBPlayerMovement>(ACharacter::CharacterMovementComponentName)) {
+	PrimaryActorTick.bCanEverTick = true;
+
+	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
+	Camera->SetupAttachment(RootComponent);
+	Camera->bUsePawnControlRotation = true;
+	Camera->SetWorldLocation(FVector(0, 0, 70));
+
+	HealthComponent = CreateDefaultSubobject<UHealthComponent>(TEXT("Health"));
+	StaminaComponent = CreateDefaultSubobject<UStaminaComponent>(TEXT("Stamina"));
+}
+
+void AAlephCharacter::BeginPlay() {
+	Super::BeginPlay();
+}
+
+void AAlephCharacter::LookUp(float Axis) {
+	AddControllerPitchInput(Axis * Sensitivity * GetWorld()->GetDeltaSeconds());
+	UpdateParallaxUI();
+}
+
+void AAlephCharacter::Turn(float Axis) {
+	AddControllerYawInput(Axis * Sensitivity * GetWorld()->GetDeltaSeconds());
+	UpdateParallaxUI();
+}
+
+void AAlephCharacter::MoveForward(float Value) {
+	if (!FMath::IsNearlyZero(Value)) {
+		AddMovementInput(GetActorForwardVector(), Value);
+	}
+}
+
+void AAlephCharacter::MoveSide(float Value) {
+	if (!FMath::IsNearlyZero(Value)) {
+		AddMovementInput(GetActorRightVector(), Value);
+	}
+}
+
+void AAlephCharacter::BeginCrouch() {
+	Crouch();
+}
+
+void AAlephCharacter::EndCrouch() {
+	UnCrouch();
+}
+
+void AAlephCharacter::BeginSprint() {
+}
+
+void AAlephCharacter::EndSprint() {
+}
+
+void AAlephCharacter::UpdateParallaxUI() {
+	PitchRate = UKismetMathLibrary::FClamp((Camera->GetComponentRotation().Pitch - CameraRot.Pitch) + PitchRate, MinMaxPitchRate * -1.0f, MinMaxPitchRate);
+	YawRate = UKismetMathLibrary::FClamp((Camera->GetComponentRotation().Yaw - CameraRot.Yaw) + YawRate, MinMaxYawRate * -1.0f, MinMaxYawRate);
+	CameraRot = Camera->GetComponentRotation();
+	PitchParallaxOffset = UKismetMathLibrary::FInterpTo(PitchParallaxOffset, PitchRate, GetWorld()->GetDeltaSeconds(), ParallaxSpeed);
+	YawParallaxOffset = UKismetMathLibrary::FInterpTo(YawParallaxOffset, YawRate, GetWorld()->GetDeltaSeconds(), ParallaxSpeed);
+
+	if (ParallaxCollection != nullptr) {
+		UKismetMaterialLibrary::SetVectorParameterValue(GetWorld(), ParallaxCollection, "Offset", FLinearColor(YawParallaxOffset, PitchParallaxOffset, 0.0f));
+	}
+
+	PitchRate = UKismetMathLibrary::FInterpTo(PitchRate, 0.0f, GetWorld()->GetDeltaSeconds(), ParallaxSpeed);
+	YawRate = UKismetMathLibrary::FInterpTo(YawRate, 0.0f, GetWorld()->GetDeltaSeconds(), ParallaxSpeed);
+}
+
+void AAlephCharacter::DisplayImGuiDebugWindow() {
+	const FString GameName = FApp::GetProjectName();
+
+	ImGui::Begin(TCHAR_TO_ANSI(*GameName), nullptr, ImGuiWindowFlags_AlwaysAutoResize);
+	ImGui::Text("FPS: %.1f", ImGui::GetIO().Framerate);
+	ImGui::Text("Velocity: %.1f", GetVelocity().Size());
+	ImGui::End();
+}
+
+void AAlephCharacter::SetDashDistance(float Distance) {
+	DashDistance = Distance * 1000.0f;
+}
+
+void AAlephCharacter::Tick(float DeltaTime) {
+	Super::Tick(DeltaTime);
+	DisplayImGuiDebugWindow();
+}
+
+void AAlephCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent) {
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+
+	PlayerInputComponent->BindAxis("LookUp", this, &AAlephCharacter::LookUp);
+	PlayerInputComponent->BindAxis("Turn", this, &AAlephCharacter::Turn);
+	PlayerInputComponent->BindAxis("Forward", this, &AAlephCharacter::MoveForward);
+	PlayerInputComponent->BindAxis("Side", this, &AAlephCharacter::MoveSide);
+
+	PlayerInputComponent->BindAction("Jump", IE_Pressed, this, &AAlephCharacter::Jump);
+	PlayerInputComponent->BindAction("Jump", IE_Released, this, &AAlephCharacter::StopJumping);
+	PlayerInputComponent->BindAction("Crouch", IE_Pressed, this, &AAlephCharacter::BeginCrouch);
+	PlayerInputComponent->BindAction("Crouch", IE_Released, this, &AAlephCharacter::EndCrouch);
+	PlayerInputComponent->BindAction("Sprint", IE_Pressed, this, &AAlephCharacter::BeginSprint);
+	PlayerInputComponent->BindAction("Sprint", IE_Released, this, &AAlephCharacter::EndSprint);
+}
---
+++ Source/Aleph/AlephCharacter.h
@@ -0,0 +1,65 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Character/PBPlayerCharacter.h"
+#include "AlephCharacter.generated.h"
+
+class UCameraComponent;
+class UHealthComponent;
+class UStaminaComponent;
+
+UCLASS()
+class ALEPH_API AAlephCharacter : public APBPlayerCharacter
+{
+	GENERATED_BODY()
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Components", meta = (AllowPrivateAccess = "true")) UCameraComponent* Camera;
+
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Components", meta = (AllowPrivateAccess = "true")) UHealthComponent* HealthComponent;
+	UPROPERTY(BlueprintReadWrite, EditAnywhere, Category = "Components", meta = (AllowPrivateAccess = "true")) UStaminaComponent* StaminaComponent;
+
+public:
+	AAlephCharacter(const FObjectInitializer& ObjectInitializer);
+
+	virtual void Tick(float DeltaTime) override;
+	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	UFUNCTION(BlueprintPure) float GetDashDistance() { return DashDistance; }
+	UFUNCTION(BlueprintCallable) void SetDashDistance(float Distance);
+
+	UPROPERTY(BlueprintReadOnly, EditAnywhere, Category = "Parallax UI") UMaterialParameterCollection* ParallaxCollection;
+
+protected:
+	virtual void BeginPlay() override;
+
+	void LookUp(float Axis);
+	void Turn(float Axis);
+
+	void MoveForward(float Value);
+	void MoveSide(float Value);
+
+	void BeginCrouch();
+	void EndCrouch();
+	void BeginSprint();
+	void EndSprint();
+
+	void UpdateParallaxUI();
+	void DisplayImGuiDebugWindow();
+
+private:
+	float Sensitivity = 20.0f;
+
+	float YawRate;
+	float PitchRate;
+	float YawParallaxOffset;
+	float PitchParallaxOffset;
+	float ParallaxSpeed = 12.0f;
+	FRotator CameraRot;
+	float MinMaxYawRate = 2.0f;
+	float MinMaxPitchRate = 12.0f;
+
+	float DashDistance;
+
+};
